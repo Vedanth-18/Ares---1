04a5499c2585ddf55f2b3fe22b6d2475f5578776
Vedanth.K <vedanthvedikka@gmail.com>
2022-06-11 15:28:53 +0530

    Update_4

--- sketch.js
+++ sketch.js
@@ -13,25 +13,37 @@ var ob2; //Position of obstacles
 var ob3; //Position of obstacles
 var ob4; //Position of obstacles
 var ob5; //Position of obstacles
+var ob6; //Position of obstacles
+var ob7; //Position of obstacles
+var ob8; //Position of obstacles
+var ob9; //Position of obstacles
+var ob10; //Position of obstacles
 var model2; //Var to load obstacle model
 var mx1; //Var to assign X position of obstacles
 var mx2; //Var to assign X position of obstacles
 var mx3; //Var to assign X position of obstacles
 var mx4; //Var to assign X position of obstacles
 var mx5; //Var to assign X position of obstacles
+var mx6; //Var to assign X position of obstacles
+var mx7; //Var to assign X position of obstacles
+var mx8; //Var to assign X position of obstacles
+var mx9; //Var to assign X position of obstacles
+var mx10; //Var to assign X position of obstacles
+const modelArray = [mx1, mx2, mx3, mx4, mx5, mx6, mx7, mx8, mx8, mx9, mx10]; //Array to add in the position/the models as a whole, to get (i).
+
+var rand3; //Testing purpose
 
 //Terrain Assests - Vars
 var M_Terrain; //Var to import 3d terrain model as obj object
 var textureImg; //Var to aplly texture for the terrain
-
+var textureImg2;
 //Terrain Creation - Vars
 var frameR; //Specialised Counting system for creating terrain synchronously with regard frameCount..
-var Tpos = 0; //Var to calculate terrain position(z-axis) of successive terrains
+var Tpos; //Var to calculate terrain position(z-axis) of successive terrains
 var Tpos2 = -200; //Var to calculate buffer terrain position(z-axis) of successive buffer terrains
 var Tpos3 = -400; //Var to calculate buffer terrain position(z-axis) of successive buffer terrains
 var Tpos4 = -600; //Var to calculate buffer terrain position(z-axis) of successive buffer terrains
 var Tpos5 = -800; //Var to calculate buffer terrain position(z-axis) of successive buffer terrains
-var Tpos6 = -1000; //Var to calculate buffer terrain position(z-axis) of successive buffer terrains
 var AcknowledgeS; //Variable to acknowledge the beginning of game[AcknowlegeS = 0, before game starts. AcknowledgeS = 1, to mark the start of the game].
 
 //Texts - Var
@@ -44,10 +56,12 @@ function preload(){
   rover = loadImage("Assets/Image/rover.gif");
   bg = loadImage("Assets/Image/Ares.webp");
   textureImg = loadImage("Assets/Image/texture1.jpg");
+  textureImg2 = loadImage("Assets/Image/texture2.jpg");
   GenralT_1 = loadFont("Assets/Text/Ares.otf")  //Free commercial license
   ARESfont = loadFont("Assets/Text/Ares.ttf");  //Free commercial license
   controlsPanel = loadImage("Assets/Image/controls.jpg");
-  model2 = loadModel("Assets/3d Object/T2.obj");
+  model2 = loadModel("Assets/3d Object/barricade.obj");
+  //model2 = loadModel("Assets/3d Object/T2.obj");
 }
 
 //Setup function
@@ -69,13 +83,21 @@ function setup() {
   //Initialising AcknowledgeS to 0[Meaning - game didn't start]
   AcknowledgeS = 0;
   //Initialising object position
-  ob1 = - 1000;
+  ob1 = -5;
   //Initialising X positions of the obstacles
-  mx1 = -80; 
-  mx2 = 1;
-  mx3 = -200;
-  mx4 = 100;
-  mx5 = -100;
+  mx1 = 40; 
+  mx2 = 24;
+  mx3 = -1;
+  mx4 = 20;
+  mx5 = -4;
+  mx6 = 15; 
+  mx7 = -3;
+  mx8 = 23;
+  mx9 = 0;
+  mx10 = 0;
+
+  //Defining Terrain Position
+  Tpos = 0;
 }
 
 function draw() {  
@@ -97,7 +119,7 @@ function draw() {
     smooth();
 
     //orbitControl
-    //orbitControl();
+    orbitControl();
 
     //Calling spawnObstacles() function to spawn the obstacles on the terrain
     spawnObstacles();
@@ -133,6 +155,7 @@ function draw() {
     if(keyIsDown(UP_ARROW) && frameCount%1 === 0 && AcknowledgeS === 1){
       frameR++;
     }
+    //console.log("FrameR: " + frameR);
     //Camera movement
     //Initialising movement when  key "S" is pressed[Aligining camera relative to rover's position]
     if(keyIsDown(83) && AcknowledgeS === 0){
@@ -146,6 +169,7 @@ function draw() {
     push();
     //Starting the game and changing rover position to its beginning when letter "S" is pressed[Aligigning rover to the  initial camera movement/position]
     if(keyIsDown(83) && AcknowledgeS === 0){
+      //roverPositionZ = 0;
       roverPositionZ = (-60);
       AcknowledgeS = 1;
     }
@@ -168,7 +192,7 @@ function draw() {
     pop();      
     }
     //Movement Controls - Panel
-    if(AcknowledgeS === 0 && keyIsDown(67)){  //Shows up when the key "C" is pressed
+    if(keyIsDown(67)){  //Shows up when the key "C" is pressed
     noStroke();
     texture(controlsPanel);
     translate(0, 0, 300);
@@ -185,14 +209,15 @@ function mousePressed(){
 //Terrain creation function - Called in main();
 function createTerrains(){
   //Calculating terrain positions
-  if(frameR > 0 && frameR%210 === 0){
+  if(frameR > 0 && frameR%350 === 0){
     //Calculating Tposition
-    Tpos =  Tpos  - 200;
+    Tpos =  Tpos  - 340;
+    //console.log("Tpos: " + Tpos);
     Tpos2 = Tpos  - 200;
     Tpos3 = Tpos2 - 200;
     Tpos4 = Tpos3 - 200;
     Tpos5 = Tpos4 - 200;
-    Tpos6 = Tpos5 - 200;
+    
  }
 
  //Terrain creation(MainTerrain)
@@ -203,6 +228,8 @@ function createTerrains(){
        //Assigning position to the terrain(z-axis position) - To appear like being continous
        translate(0,0,Tpos);
        //Removing strokes on the terrain to make it look plain
+       //fill("YELLOW");
+       //stroke("BLACK");
        noStroke();
        //Applying mars like texture to the 3d model
        texture(textureImg);
@@ -216,6 +243,8 @@ function createTerrains(){
        scale(15);
        translate(0,0,Tpos2);
        noStroke();
+       //fill("RED");
+       //stroke("BLACK");
        texture(textureImg);
        model(M_Terrain);
        pop();
@@ -225,6 +254,8 @@ function createTerrains(){
        push();
        scale(15);
        translate(0,0,Tpos3);
+       //fill("BLUE");
+       //stroke("BLACK");
        noStroke();
        texture(textureImg);
        model(M_Terrain);
@@ -240,7 +271,7 @@ function createTerrains(){
        model(M_Terrain);
        pop();
 
-    //Terrain creation(BufferTerrain6)
+    //Terrain creation(BufferTerrain4)
     //M_Terrain(n) - Properties of terrain(n) - BufferTerrain4..
        push();
        scale(15);
@@ -251,72 +282,129 @@ function createTerrains(){
        model(M_Terrain);
        pop();
 
-    //Terrain creation(BufferTerrain5)
-    //M_Terrain(n) - Properties of terrain(n) - BufferTerrain5..
-    push();
-    scale(15);
-    translate(0,0,Tpos6);
-    noStroke();
-    fill(255, 102, 94);
-    texture(textureImg);
-    model(M_Terrain);
-    pop();
+       
 }
 
 //Function for Spawing the obstacles around on the terrain
  function spawnObstacles(){
-  if(frameR > 0 && frameR%390 === 0){
-   //Calculating object position
-   ob1 = ob1 - 6000
-   }
-   ob2 = ob1 - 2000;
-   ob3 = ob2 - 2000;
-   ob4 = ob3 - 2000;
-   ob5 = ob4 - 2000;
+   if(frameR > 0 && frameR%370 === 0){
+     //console.log("Tpos2: " + Tpos);
+    //Calculating object position
+    ob1 = ob1 - 340;
+  }
+    ob2 = ob1 - 96;
+    ob3 = ob2 - 96;
+    ob4 = ob3 - 96;
+    ob5 = ob4 - 96;
+    ob6 = ob5 - 96;
+    ob7 = ob6 - 96;
+    ob8 = ob7 - 96;
+    ob9 = ob8 - 96;
+    ob10 = ob9- 96;
+   
+    //console.log("ob1: " + ob1);
+    //console.log("Tpos; " + Tpos);
+   //var ratio = ob1/frameR;
+   //console.log("ob1: " + ob1);
+   //console.log("Ratio: " + ratio)
+   //console.log("frameR: " + frameR);
   
-if(frameR>0 && frameR%390 === 0){
-  mx1 = (random(-250, -50)); 
-  mx2 = (random(0, -150));
-  mx3 = (random(50, 250));
-  mx4 = (random(0, 250));
-  mx5 = (random(-250, -100));
+if(frameR>0 && frameR%140 === 0){
+  mx1 = (random(-4, 27)); 
+  mx2 = (random(10, 22));
+  mx3 = (random(-4, 6));
+  mx4 = (random(-1, 20));
+  mx5 = (random(-4, 18));
+  mx6 = (random(10, 22)); 
+  mx7 = (random(0, 27));
+  mx8 = (random(0, 27));
+  mx9 = (random(-2, 27));
+  mx10 = (random(1, 27));
 }
+
+ //console.log("Length: " + modelArray.length);
 //for(var i = 0; i<1; i++){
   push();
+  scale(15);
   fill("WHITE");
+  translate(mx1, 17.9, ob1);
   noStroke();
-  translate(mx1, 80, ob1);
+  //texture(textureImg);
   model(model2);
   pop();
  
   push();
-  fill("WHITE");
+  scale(15);
+  //fill("WHITE");
+  translate(mx2, 17.9, ob2);
   noStroke();
-  translate(mx2, 80, ob2);
+  //texture(textureImg);
   model(model2);
   pop();
  
   push();
+  scale(15);
   fill("WHITE");
   noStroke();
-  translate(mx3, 80, ob3);
+  translate(mx3, 17.9, ob3);
   model(model2);
   pop();
  
   push();
+  scale(15);
   fill("WHITE");
   noStroke();
-  translate(mx4, 80, ob4);
+  translate(mx4, 17.9, ob4);
   model(model2);
   pop();
  
   push();
+  scale(15);
   fill("WHITE");
   noStroke();
-  translate(mx5, 80, ob5);
+  translate(mx5, 17.9, ob5);
   model(model2);
   pop();
-//}
+
+  push();
+  scale(15);
+  fill("WHITE");
+  noStroke();
+  translate(mx6, 17.9, ob6);
+  model(model2);
+  pop();
+
+  push();
+  scale(15);
+  fill("WHITE");
+  noStroke();
+  translate(mx7, 17.9, ob7);
+  model(model2);
+  pop();
+
+  push();
+  scale(15);
+  fill("WHITE");
+  noStroke();
+  translate(mx8, 17.9, ob8);
+  model(model2);
+  pop();
+
+  push();
+  scale(15);
+  fill("WHITE");
+  noStroke();
+  translate(mx9, 17.9, ob9);
+  model(model2);
+  pop();
+
+  push();
+  scale(15);
+  fill("WHITE");
+  noStroke();
+  translate(mx10, 17.9, ob10);
+  model(model2);
+  pop();
 }
 
 
@@ -548,3 +636,75 @@ if(frameR>0 && frameR%390 === 0){
   
 // }
   
+
+
+
+
+///.....................EXTRA TERRAINS........................//
+//Terrain creation(BufferTerrain5)
+       //M_Terrain(n) - Properties of terrain(n) - BufferTerrain5..
+       //push();
+       //scale(15);
+       //translate(0,0,Tpos6);
+       //noStroke();
+       //fill(255, 102, 94);
+       //texture(textureImg);
+       //model(M_Terrain);
+       //pop(); 
+//
+       ////Terrain creation(BufferTerrain6)
+       ////M_Terrain(n) - Properties of terrain(n) - BufferTerrain6..
+       //push();
+       //scale(15);
+       //translate(0,0,Tpos7);
+       //noStroke();
+       //fill(255, 102, 94);
+       //texture(textureImg);
+       //model(M_Terrain);
+       //pop();
+//
+       ////Terrain creation(BufferTerrain7)
+       ////M_Terrain(n) - Properties of terrain(n) - BufferTerrain7..
+       //push();
+       //scale(15);
+       //translate(0,0,Tpos8);
+       //noStroke();
+       //fill(255, 102, 94);
+       //texture(textureImg);
+       //model(M_Terrain);
+       //pop();
+//
+       ////Terrain creation(BufferTerrain7)
+       ////M_Terrain(n) - Properties of terrain(n) - BufferTerrain8..
+       //push();
+       //scale(15);
+       //translate(0,0,Tpos9);
+       //noStroke();
+       //fill(255, 102, 94);
+       //texture(textureImg);
+       //model(M_Terrain);
+       //pop();
+//
+       ////Terrain creation(BufferTerrain9)
+       ////M_Terrain(n) - Properties of terrain(n) - BufferTerrain9..
+       //push();
+       //scale(15);
+       //translate(0,0,Tpos10);
+       //noStroke();
+       //fill(255, 102, 94);
+       //texture(textureImg);
+       //model(M_Terrain);
+       //pop();
+       ////////////INITIALISATION CODE...........
+       //Tpos6 = Tpos5 - 200;
+    //Tpos7 = Tpos6 - 200;
+    //Tpos8 = Tpos7 - 200;
+    //Tpos9 = Tpos + 200;
+    //Tpos10 = Tpos9 + 200;
+    ////
+    // var Tpos6 = -1000; //Var to calculate buffer terrain position(z-axis) of successive buffer terrains
+// var Tpos7 = -1200;
+// var Tpos8 = -1400;
+// var Tpos9 = 200;
+// var Tpos10 = 400;
+///////////////.........................../////////////////////////
